9540f069a94093aba126acd3bb64d84cb58b800c
gab-che <gabriellachessa1@gmail.com>
2022-11-23 20:02:27 +0100

    adds category suggestions

--- js/script.js
+++ js/script.js
@@ -7,21 +7,26 @@ createApp({
                 {
                     text: "Imparare a usare Vue Js",
                     done: false,
+                    badge: "Codice",
                 }, {
                     text: "Capire se Vue Js è veramente un pacchetto",
                     done: false,
+                    badge: "Codice",
                 }, {
                     text: "Leggere Horcynus Orca",
                     done: false,
+                    badge: "Libri",
                 }, {
                     text: "Leggere il flacone del Prozac",
                     done: false,
+                    badge: "Salute",
                 }
             ],
 
             newTask : {
                 text: "",
-            }
+                badge: "",
+            },
         }
     },
 
@@ -31,10 +36,30 @@ createApp({
         },
 
         taskAdd(){
-            this.taskList.push({
-                text : this.newTask.text,
-                done: false,
-            });
+            if(!this.newTask.text || !this.newTask.badge){
+                alert("Compila il campo mancante!")
+            } else{
+                this.taskList.push({
+                    text : this.newTask.text,
+                    done: false,
+                    badge: this.newTask.badge,
+                })
+
+            };
+
+            this.newTask.text = ''
+            this.newTask.badge = ''
         },
+
+        fetchCategories(){
+            categories = []
+            this.taskList.forEach((element)=>{
+                if(!categories.includes(element.badge)){
+                    categories.push(element.badge)
+                }
+            })
+
+            return categories
+        }
     }
 }).mount("#app");
